897c8d4b8706ed70cbb32e67357e3fb59b9f3c2f
mmolta <mcmolta10@gmail.com>
2020-07-21 12:15:35 -0400

    add table and begin table styles

--- js/index.js
+++ js/index.js
@@ -1,22 +1,25 @@
 // Homepage Interactions
 const nav = document.getElementById('nav-header')
 const iconsWrapper = document.getElementById('indicator-icons-wrapper')
+let iconsSticky = false
 
 // these elements don't always need to be sticky because they don't always exist, and when they don't, the scroll doesn't happen
 // invoke this function whenever 
+// @TODO: when the function to generate indicator content is made, delete this function and just apply the height when the elements are created
 const makeSticky = () => {
     const tabs = document.getElementById('tpm-content-headers')
     const headerActive = document.querySelector('.content-header')
 
     // get height for top
     const navHeight = nav.clientHeight
-    console.log(navHeight)
     const iconsHeight = iconsWrapper.clientHeight
     const comboHeight = navHeight + iconsHeight - 1
 
-    // set top
-    iconsWrapper.style.position = 'sticky'
-    iconsWrapper.style.top = navHeight + 'px'
+    // set top (first pass only)
+    if(!iconsSticky) {
+        iconsWrapper.style.position = 'sticky'
+        iconsWrapper.style.top = navHeight + 'px'
+    }
 
     // double check indicator content exists before applying styles to it
     if(tabs) {
