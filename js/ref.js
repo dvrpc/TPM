bf359268f7eb35ae204f6ee2477e829a85a08215
mmolta <mcmolta10@gmail.com>
2020-07-21 14:22:37 -0400

    begin creating ref for indicator content + update table styles

---
+++ js/ref.js
@@ -0,0 +1,125 @@
+const ref = {
+    'highway safety': {
+        'measure': `
+            <h2 class="content-header">What do we Measure?</h2>
+            <p>State Departments of Transportation (DOTs) and Metropolitan Planning Organizations (MPOs) must set annual targets for five safety-related performance measures, with the goal of achieving a significant reduction in traffic fatalities and serious injuries on all public roads. State DOTs report baseline values, targets, and progress toward reducing the number and rate of fatalities, the number and rate of serious injuries, and the number of nonmotorized fatalities and serious injuries to FHWA in an annual safety report. MPOs may either establish quantitative targets for their metropolitan planning area or agree to adopt the statewide targets.</p>
+            <p>FHWA requires the DOTs and MPOs to establish safety targets on an annual basis for:</p>
+            <ul>
+                <li><strong>Number of Fatalities:</strong> The total number of persons suffering fatal injuries in a motor vehicle crash during a calendar year.</li>
+                <li><strong>Rate of Fatalities:</strong> The ratio of total number of fatalities to the number of vehicle miles traveled (VMT, in 100 Million VMT) in a calendar year.</li>
+                <li><strong>Number of Serious Injuries:</strong> The total number of persons suffering at least one serious injury in a motor vehicle crash during a calendar year.</li>
+                <li><strong>Rate of Serious Injuries:</strong> The ratio of total number of serious injuries to the number of VMT (in 100 Million VMT) in a calendar year.</li>
+                <li><strong>Number of Non-motorized Fatalities and Non-motorized Serious Injuries:</strong> The combined total number of non-motorized fatalities and non-motorized serious injuries involving a motor vehicle during a calendar year.</li>
+            </ul>
+            <p>Measures are for all public roads regardless of functional classification or ownership and each target is based on a five-year rolling average, which is the average of five individual, consecutive points of data.</p>
+        `,
+        'targets': `
+            <h3 class="tpm-content-subheaders">New Jersey</h3>
+            <div class="table-overflow-wrapper">
+                <table>
+                    <thead>
+                        <th class="table-text-cell">Measure</th>
+                        <th class="table-num-cell">NJ Target <br /> (2016 - 2020)</th>
+                        <th class="table-num-cell">NJ Baseline <br /> (2014 - 2018)</th>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="table-text-cell">Number of Fatalities</td>
+                            <td class="table-num-cell">582.2</td>
+                            <td class="table-num-cell">581.6</td>
+                        </tr>
+                        <tr>
+                            <td class="table-text-cell">Rise in Fatalities (per 100 million VMT)</td>
+                            <td class="table-num-cell">0.744</td>
+                            <td class="table-num-cell">0.759</td>
+                        </tr>
+                        <tr>
+                            <td class="table-text-cell">Number of Serious Injuries</td>
+                            <td class="table-num-cell">1,167.9</td>
+                            <td class="table-num-cell">1,110.8</td>
+                        </tr>
+                        <tr>
+                            <td>Rate of Serious Injuries (per 100 million VMT)</td>
+                            <td class="table-num-cell">1.489</td>
+                            <td class="table-num-cell">1.449</td>
+                        </tr>
+                        <tr>
+                            <td class="table-text-cell">Number of Non-Motorized Fatalities and Serious Injuries</td>
+                            <td class="table-num-cell">407.9</td>
+                            <td class="table-num-cell">392.7</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <small>Source: DVRPC 2020</small>
+            <h3 class="tpm-content-subheaders">Pennsylvania</h3>
+            <div class="table-overflow-wrapper">
+                <table>
+                    <thead>
+                        <th class="table-text-cell">Measure</th>
+                        <th class="table-num-cell">PA Target <br /> (2016 - 2020)</th>
+                        <th class="table-num-cell">PA Baseline <br /> (2014 - 2018)</th>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="table-text-cell">Number of Fatalities</td>
+                            <td class="table-num-cell">1,171.9</td>
+                            <td class="table-num-cell">1,182</td>
+                        </tr>
+                        <tr>
+                            <td class="table-text-cell">Rise in Fatalities (per 100 million VMT)</td>
+                            <td class="table-num-cell">1.148</td>
+                            <td class="table-num-cell">1.169</td>
+                        </tr>
+                        <tr>
+                            <td class="table-text-cell">Number of Serious Injuries</td>
+                            <td class="table-num-cell">4,400.3</td>
+                            <td class="table-num-cell">3,839.6</td>
+                        </tr>
+                        <tr>
+                            <td>Rate of Serious Injuries (per 100 million VMT)</td>
+                            <td class="table-num-cell">4.309</td>
+                            <td class="table-num-cell">3.797</td>
+                        </tr>
+                        <tr>
+                            <td class="table-text-cell">Number of Non-Motorized Fatalities and Serious Injuries</td>
+                            <td class="table-num-cell">781.7</td>
+                            <td class="table-num-cell">679</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <small>source: DVRPC 2020</small>
+        `,
+        'how': `
+        
+        `,
+        'dates': ``
+    },
+    'infrastructure': {
+        'measure': ``,
+        'targets': ``,
+        'how': ``,
+        'dates': ``
+    },
+    'reliability': {
+        'measure': ``,
+        'targets': ``,
+        'how': ``,
+        'dates': ``
+    },
+    'assets': {
+        'measure': ``,
+        'targets': ``,
+        'how': ``,
+        'dates': ``
+    },
+    'safety 2': {
+        'measure': ``,
+        'targets': ``,
+        'how': ``,
+        'dates': ``
+    },
+}
+
+export default ref
