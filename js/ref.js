7ef455ebcca93085d98014769a796d272797cfe2
mmolta <mcmolta10@gmail.com>
2020-09-02 15:07:23 -0400

    add proofread highway safety content

--- js/ref.js
+++ js/ref.js
@@ -2,7 +2,7 @@ const ref = {
     'highway safety': {
         'measure': `
             <h2 class="content-header">What do We Measure?</h2>
-            <p>State Departments of Transportation (DOTs) and Metropolitan Planning Organizations (MPOs) must set annual targets for five safety-related performance measures, with the goal of achieving a significant reduction in traffic fatalities and serious injuries on all public roads. State DOTs report baseline values, targets, and progress toward reducing the number and rate of fatalities, the number and rate of serious injuries, and the number of nonmotorized fatalities and serious injuries to FHWA in an annual safety report. MPOs may either establish quantitative targets for their metropolitan planning area or agree to adopt the statewide targets.</p>
+            <p>State Departments of Transportation (DOTs) and Metropolitan Planning Organizations (MPOs) must set annual targets for five safety-related performance measures, with the goal of achieving a significant reduction in traffic fatalities and serious injuries on all public roads. State DOTs report baseline values, targets, and progress toward reducing the number and rate of fatalities, the number and rate of serious injuries, and the number of non-motorized fatalities and serious injuries to FHWA in an annual safety report. MPOs may either establish quantitative targets for their metropolitan planning area or agree to adopt the statewide targets. </p>
             <p>FHWA requires the DOTs and MPOs to establish safety targets on an annual basis for:</p>
             <ul>
                 <li><strong>Number of Fatalities:</strong> The total number of persons suffering fatal injuries in a motor vehicle crash during a calendar year.</li>
@@ -26,7 +26,7 @@ const ref = {
                     <tbody>
                         <tr>
                             <td class="table-cell-first">Number of Fatalities</td>
-                            <td>582.2</td>
+                            <td>582.8</td>
                             <td class="table-cell-last">581.6</td>
                         </tr>
                         <tr>
@@ -94,15 +94,15 @@ const ref = {
         `,
         'how': `
             <h2 class="content-header">How are We Doing?</h2>
-            <p>State DOTs report baseline values, targets, and progress toward meeting the targets to FHWA in an annual safety report. FHWA will determine whether a State has met or made significant progress toward its safety performance targets. A State is considered to have met or made significant progress when at least four out of the five safety performance targets are met or the actual outcome for the safety performance target is better than baseline performance.</p>
+            <p>state DOTs report baseline values, targets, and progress toward meeting the targets to FHWA in an annual safety report. FHWA will determine whether a state has met or made significant progress toward its safety performance targets. A state is considered to have met or made significant progress when at least four out of the five safety performance targets are met or the actual outcome for the safety performance target is better than baseline performance.</p>
             <h3 class="tpm-content-subheaders">Meeting a Target:</h3>
             <span>If the actual outcome for a performance measure is equal to or less than the target, the target has been met.</span>
             <h3 class="tpm-content-subheaders">Better than Baseline</h3>
-            <span>If a State has not met a target, FHWA will determine if the actual outcome for the target is better than the baseline performance for that target. The baseline performance is the 5-year rolling average for the target ending the year prior to the establishment of the State's target.</span>
-            <p>If a State has not met or made significant progress toward meeting its targets, the State must comply with the provisions set forth in 23 USC 148(i) for the subsequent fiscal year. The State shall:</p>
+            <span>If a state has not met a target, FHWA will determine if the actual outcome for the target is better than the baseline performance for that target. The baseline performance is the five-year rolling average for the target ending the year prior to the establishment of the state's target.</span>
+            <p>If a state has not met or made significant progress toward meeting its targets, the state must comply with the provisions set forth in 23 USC 148(i) for the subsequent fiscal year. The state shall:</p>
             <ol>
                 <li>Use obligation authority equal to the Highway Safety Improvement Program (HSIP) apportionment for the year prior to the target year, only for HSIP projects.</li>
-                <li>Submit an HSIP Implementation Plan that describes actions the State will take to meet or make significant progress toward meeting its targets. The HSIP Implementation Plan should guide the State's project decisions so that the combined 148(i) provisions lead to the State meeting or making significant progress toward meeting its safety performance targets in subsequent years.                        </li>
+                <li>Submit an HSIP Implementation Plan that describes actions the state will take to meet or make significant progress toward meeting its targets. The HSIP Implementation Plan should guide the state's project decisions so that the combined 148(i) provisions lead to the state meeting or making significant progress toward meeting its safety performance targets in subsequent years.</li>
             </ol>
             <h3 class="tpm-content-subheaders">New Jersey</h3>
             <div class="table-overflow-wrapper">
@@ -166,7 +166,7 @@ const ref = {
                 </table>
             </div>
             <small>Source: DVRPC 2020</small><br />
-            <small>* 4 out of 5 targets must be met, or have better performance than the baseline</small>
+            <small>*Four out of five targets must be met, or have better performance than the baseline.</small>
             <p>New Jersey has demonstrated that it has met or made significant progress towards meeting annual safety goals for calendar year (CY) 2018.</p>
             <h3 class="tpm-content-subheaders">Pennsylvania</h3>
             <div class="table-overflow-wrapper">
@@ -230,8 +230,8 @@ const ref = {
                 </table>
             </div>
             <small>Source: DVRPC 2020</small><br />
-            <small>* 4 out of 5 targets must be met, or have better performance than the baseline</small>
-            <p>Pennsylvania did not meet or make significant progress towards its safety performance targets on three out of five performance measures: Number of Serious Injuries, Rate of Serious Injuries, and Number of Non-Motorized Fatalities and Serious Injuries for CY 2018. PennDOT is now required to submit and HSIP Implementation Plan. This plan is currently being written and is scheduled to be delivered to FHWA by the June 30, 2020 deadline. PennDOT is also required to use obligation authority equal to the HSIP apportionment for FY2018, which is $97,585,000, for safety projects in the upcoming fiscal year.</p>
+            <small>*Four out of Five targets must be met, or have better performance than the baseline.</small>
+            <p>Pennsylvania did not meet or make significant progress towards its safety performance targets on three out of five performance measures: Number of Serious Injuries, Rate of Serious Injuries, and Number of Non-Motorized Fatalities and Serious Injuries for CY 2018. PennDOT is now required to submit an HSIP Implementation Plan. This plan is currently being written and is scheduled to be delivered to FHWA by the June 30, 2020 deadline. PennDOT is also required to use obligation authority equal to the HSIP apportionment for FY2018, which is $97,585,000, for safety projects in the upcoming fiscal year.</p>
         `,
         'dates': `
             <h2 class="content-header">Key Dates and Resources</h2>
@@ -242,29 +242,30 @@ const ref = {
                 </div>
                 <div class="dates-entry dates-entry-right">
                     <span class="dates-circle dates-circle-right"></span>
-                    <p class="dates-entry-text"><strong>April 2016</strong><br />State DOTs report CY 2018 Targets on HSIP Annual Report to FHWA (Annually thereafter).</p>
+                    <p class="dates-entry-text"><strong>August 2017</strong><br />State DOTs report CY 2018 targets on HSIP Annual Report to FHWA (annually thereafter).</p>
                 </div>
                 <div class="dates-entry dates-entry-left">
                     <span class="dates-circle dates-circle-left"></span>
-                    <p class="dates-entry-text"><strong>April 2016</strong><br />DVRPC establishes targets for the Safety Performance Measures for the first time (Annually thereafter).</p>
+                    <p class="dates-entry-text"><strong>February 2018</strong><br />DVRPC establishes targets for the Safety Performance Measures for the first time (annually thereafter).</p>
                 </div>
                 <div class="dates-entry dates-entry-right">
                     <span class="dates-circle dates-circle-right"></span>
-                    <p class="dates-entry-text"><strong>April 2016</strong><br />DVRPC reports on progress toward CY 2018 targets (Annually thereafter).</p>
+                    <p class="dates-entry-text"><strong>February 2020</strong><br />DVRPC reports on progress toward CY 2018 targets (annually thereafter).</p>
                 </div>
                 <div class="dates-entry dates-entry-left">
                     <span class="dates-circle dates-circle-left"></span>
-                    <p class="dates-entry-text"><strong>June 30, 2020</strong><br />If a State has not met or made significant progress toward meeting its targets, the State must submit an HSIP Implementation Plan that describes actions the State will take to meet or make significant progress toward meeting its targets.</p>
+                    <p class="dates-entry-text"><strong>June 30, 2020</strong><br />If a State has not met or made significant progress toward meeting its targets, the state must submit an HSIP Implementation Plan that describes actions the state will take to meet or make significant progress toward meeting its targets.</p>
                 </div>
             </article>
             
             <h3 class="tpm-content-subheaders">Resources</h3>
             <ul>
-                <li><a href="https://www.fhwa.dot.gov/tpm" target="blank" rel="noopener">FHWA TPM website</a></li>
-                <li><a href="https://safety.fhwa.dot.gov/hsip/spm/" target="blank" rel="noopener">FHWA Safety Performance Management webpage</a></li>
-                <li><a href="https://www.fhwa.dot.gov/tpm/reporting/state/" target="blank" rel="noopener">State Performance Dashboard and Reports</a></li>
-                <li><a href="https://www.penndot.gov/TravelInPA/Safety/Pages/Strategic-Highway-Safety-Plan.aspx" target="blank" rel="noopener">Pennsylvania Strategic Highway Safety Plan</a></li>
-                <li><a href="https://www.federalregister.gov/documents/2016/03/15/2016-05202/national-performance-management-measures-highway-safety-improvement-program" target="blank" rel="noopener">Safety Performance Measure Final Rule</a></li>
+                <li><a href="https://www.fhwa.dot.gov/tpm" target="blank" rel="noopener noreferrer">FHWA TPM website</a></li>
+                <li><a href="https://safety.fhwa.dot.gov/hsip/spm/" target="blank" rel="noopener noreferrer">FHWA Safety Performance Management webpage</a></li>
+                <li><a href="https://www.fhwa.dot.gov/tpm/reporting/state/" target="blank" rel="noopener noreferrer">State Performance Dashboard and Reports</a></li>
+                <li><a href="https://www.penndot.gov/TravelInPA/Safety/Pages/Strategic-Highway-Safety-Plan.aspx" target="blank" rel="noopener noreferrer">Pennsylvania Strategic Highway Safety Plan</a></li>
+                <li><a href="https://www.saferoadsforallnj.com" target="blank" rel="noopener noreferrer">New Jersey Strategic Highway Safety Plan</a>
+                <li><a href="https://www.federalregister.gov/documents/2016/03/15/2016-05202/national-performance-management-measures-highway-safety-improvement-program" target="blank" rel="noopener noreferrer">Safety Performance Measure Final Rule</a></li>
             </ul>
         `
     },
