5a94a182f809e8c11d54790ffb12be29c23adfee
mmolta <mcmolta10@gmail.com>
2020-12-15 16:20:32 -0500

    initialize npms project + install webpack & begin creating webpack config

---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+// configure index.html
+let indexConfig = new HtmlWebpackPlugin({
+    template: path.resolve(__dirname + "/index.html"),
+    file: 'index.html',
+    inject: 'body',
+    minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: false,
+        removeStyleLinkTypeAttributes: false,
+        useShortDoctype: true
+    }
+})
+
+module.exports = {
+    entry: ['./js/index.js'],
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['minify']
+                }
+            },
+            // load styles
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            // load imgs
+            {
+                test: /\.(png|svg)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'img/[name].[ext]'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'build')
+    },
+    plugins: [
+        new CopyWebpackPlugin(
+            [
+                {
+                    from: './img',
+                    to: 'img',
+                    toType: 'dir'
+                }
+                {
+                    from: './css',
+                    to: 'css',
+                    toType: 'dir'
+                }
+            ]
+        ),
+    // HtmlWebpackPlugin
+        indexConfig
+    ]
+}
