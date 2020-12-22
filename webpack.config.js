775b7c0cbfdfdd017013d728eee4bad31219da70
mmolta <mcmolta10@gmail.com>
2020-12-22 09:25:59 -0500

    update build minifier

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
 
 // configure index.html
 let indexConfig = new HtmlWebpackPlugin({
@@ -54,7 +55,15 @@ module.exports = {
     optimization: {
         minimize: true,
         minimizer: [
-            new CssMinimizerPlugin()
+            new CssMinimizerPlugin(),
+            new TerserPlugin({
+                terserOptions: {
+                    format: {
+                        comments: false,
+                    },
+                },
+                extractComments: false,
+            })
         ]
     },
     output: {
